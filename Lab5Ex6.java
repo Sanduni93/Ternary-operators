4e0e0b37eb5440eb539e7dc8a9513fd251918772
Sanduni93 <nirodhajayaweera1993@gmail.com>
2018-07-25 20:49:59 +0530

    Decide whether it is even or odd number

---
+++ Lab5Ex6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Lab5Ex6{
+	public static void main(String args[]){
+	Scanner scn = new Scanner(System.in);
+
+    System.out.print("Enter number:");	
+	int a= scn.nextInt();
+	
+if(a%2 == 0){
+	   System.out.println("Even number");	
+	
+}else{
+	
+	   System.out.print("Odd number");	
+}
+		
+	}
+	
+	
+}
